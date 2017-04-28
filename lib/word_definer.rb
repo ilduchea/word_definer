eb48de8ba6e0be8b72369ebc8cfd74e41d9eae55
Ilduchea <Ilduchea>
2017-04-28 08:49:25 -0700

    add spec and method to recall all words. Passing

--- lib/word_definer.rb
+++ lib/word_definer.rb
@@ -1,8 +1,17 @@
 class Word_definer
+  @@words = []
   attr_accessor(:word)
 
   define_method(:initialize) do |atributes|
     @word = atributes.fetch(:word)
   end
+
+  def self.all
+    @@words
+  end
+
+  # def id
+  #   @id
+  # end
 
 end
--- spec/word_definer_spec.rb
+++ spec/word_definer_spec.rb
@@ -3,7 +3,20 @@ require 'rspec'
 require 'pry'
 
 describe 'Word_definer' do
-  it 'intializes a new word to be defined' do    
+  it 'intializes a new word to be defined' do
     expect(word = Word_definer.new({:word => "test"})).to eq (word)
   end
+
+  describe '.all' do
+    it 'returns an empty array initially' do
+      expect(Word_definer.all).to eq ([])
+    end
+  end
+
+  # describe '#id' do
+  #   it 'returns a unique id for each instance of Word_definer' do
+  #     word = Word_definer.new({:word => "test"})
+  #     expect(word.id).to eq ("word1")
+  #   end
+  # end
 end
